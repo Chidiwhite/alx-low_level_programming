ad883ee38c23f298d9a03abc2ee714a17cb37f49
Chidiwhite <joshuachidiebere6@gmail.com>
2022-09-15 22:09:22 -0700

    5-more-numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@ void more_numbers(void)
 {
 	int num, count;
 
-	for (count = 0; count <= 14; count++)
+	for (count = 0; count <= 10; count++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num > 9)
+			if (num >= 10)
 				_putchar((num / 10) + '0');
 
 			_putchar((num % 10) + '0');
