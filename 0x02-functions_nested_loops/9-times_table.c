dfcda9045c2729cd0e3712d6905aa158c35bf0b7
Chidiwhite <joshuachidiebere6@gmail.com>
2022-09-28 11:51:44 -0700

    9-times-table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,15 +8,46 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, p;
+	int k = 0;
 
-	for (j = 0; j < 10; j++)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 10; i++)
+		for (j = 0; j < 11; j++)
 		{
-			_putchar((i * j) + 0);
-			_putchar(',');
+			p = j * k;
+			if (p >= 0 && p <= 9)
+			{
+				if (j == 0)
+					_putchar(p + '0');
+				else if (j == 10)
+				{
+					_putchar('\n');
+					k++;
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(p + '0');
+				}
+			}
+			else if (p >= 10)
+			{
+				if (j == 10)
+				{
+					_putchar('\n');
+					k++;
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((p / 10) + '0');
+					_putchar((p % 10) + '0');
+				}
+			}
 		}
-		_putchar('\n');
 	}
 }
