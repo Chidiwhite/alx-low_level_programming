ac03acb58950b30fd0d675e979f56b852d1ee6eb
Chidiwhite <joshuachidiebere6@gmail.com>
2022-09-28 01:48:27 -0700

    11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,36 +6,35 @@
  * @n: number input
  * Return: Always 0 (Success)
  */
-void print_to_98(int n) 
+void print_to_98(int n)
 {
-	for (n = 0; n <= 98; ++n)
+	if (n < 98)
 	{
-		if (n < 98)
-			printf("%d, ", n);
-		else
+		while (n <= 98)
+		{
 			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
 	}
-	printf("\n");
-	printf("98\n");
-	        
-	for (n = 111; n >= 98; n--)
-		if (n > 98)
-			printf("%d, ", n);
-		else if (n >= 98)
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
 			printf("%d", n);
-	printf("\n");
-
-	for (n = 81; n <= 98; n++)
-		if (n < 98)
-			printf("%d, ", n);
-		else if (n <= 98)
-			printf("%d", n);
-	printf("\n");
-
-	for (n = -10; n <= 98; n++)
-		if (n < 98)
-			printf("%d, ", n);
-		else if (n >= 98)
-			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
 	printf("\n");
 }
